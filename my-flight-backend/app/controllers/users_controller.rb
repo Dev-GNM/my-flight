8b36641024a1245c9363094aea7dc43214a74be9
Geoffrey <geoffreynjihia15@gmail.com>
2022-10-11 16:45:28 +0300

    added controller actions

--- my-flight-backend/app/controllers/users_controller.rb
+++ my-flight-backend/app/controllers/users_controller.rb
@@ -3,39 +3,50 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
-
-    render json: @users
+    render json: User.all, status: :ok
   end
 
   # GET /users/1
   def show
-    render json: @user
+    user = User.find_by(:id:params[:id])
+    if user
+    render json: user, status: :ok
+    else
+      render json: { error: "User not found"}, status: :not_found
+    end
   end
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    user = User.create(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if user.valid?
+      render json: user, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      render json: @user
+    user = User.find_by(id:params[:id])
+    if user
+    user.update(user_params)
+      render json: user, status: :accepted
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { error: "User not found" }, status: :not_found
     end
   end
 
   # DELETE /users/1
   def destroy
-    @user.destroy
+    user = User.find(id:params[:id])
+    if user
+    user.destroy
+    head :no_content
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
   end
 
   private
