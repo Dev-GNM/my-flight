0c3bd29c41f4073e3e79bce1be6024cc5d749344
Geoffrey <geoffreynjihia15@gmail.com>
2022-10-12 18:19:30 +0300

    added routes

--- my-flight-backend/config/routes.rb
+++ my-flight-backend/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :airlines
-  resources :users, only: [:index, :show, :create, :update, :destroy]
+  root 'pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, params: :slug
+      resources :reviews, only: [:create, :destroy]
+      resources :users, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+
+  get '*path', to: 'pages#index', via: :all
 end
